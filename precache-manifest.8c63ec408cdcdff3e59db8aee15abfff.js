63824eb731270abe4154cdcd290260b88d7bb2ba
Elias Ramsberg <elias.ramsberg@students.fhnw.ch>
2023-05-17 03:37:10 +0100

    Updates

---
+++ precache-manifest.8c63ec408cdcdff3e59db8aee15abfff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5e2f9bb048fc62ff128cb8a605b9a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf70c67dd3088dad3cb",
+    "url": "/static/css/2.e8c11978.chunk.css"
+  },
+  {
+    "revision": "3d0cac538b160a93b423",
+    "url": "/static/css/main.eb621e46.chunk.css"
+  },
+  {
+    "revision": "ecf70c67dd3088dad3cb",
+    "url": "/static/js/2.6f29b73f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6f29b73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0cac538b160a93b423",
+    "url": "/static/js/main.3bae9955.chunk.js"
+  },
+  {
+    "revision": "383eb105be49489a3f0c",
+    "url": "/static/js/runtime-main.6439aed6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.7c8490fb5f457322960dc892b369cc74.js"
+  "/precache-manifest.8c63ec408cdcdff3e59db8aee15abfff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
