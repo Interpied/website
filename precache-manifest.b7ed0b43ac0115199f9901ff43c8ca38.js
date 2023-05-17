1cfc11e2f498c295368d9b739ac7664a6f3c9bbb
Elias Ramsberg <elias.ramsberg@students.fhnw.ch>
2023-05-17 01:48:12 +0100

    Updates

---
+++ precache-manifest.b7ed0b43ac0115199f9901ff43c8ca38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671b4a7c6dacc6e6c7ca18fdf773ce1d",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "c4b3a19edd52b1489eac",
+    "url": "/website/static/css/2.e8c11978.chunk.css"
+  },
+  {
+    "revision": "45e5b3e9faa86a2ede06",
+    "url": "/website/static/css/main.eb621e46.chunk.css"
+  },
+  {
+    "revision": "c4b3a19edd52b1489eac",
+    "url": "/website/static/js/2.b671f591.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/website/static/js/2.b671f591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e5b3e9faa86a2ede06",
+    "url": "/website/static/js/main.d68577f3.chunk.js"
+  },
+  {
+    "revision": "2dd183cbbc69939053bc",
+    "url": "/website/static/js/runtime-main.47ffd3b6.js"
+  }
+]);
