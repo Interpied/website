8b999d97d48548a239f3161a222861c94727c1f1
Elias Ramsberg <elias.ramsberg@students.fhnw.ch>
2023-05-21 03:15:44 +0100

    version change, redirect

--- src/components/pages/Misc.js
+++ src/components/pages/Misc.js
@@ -5,7 +5,7 @@ function Misc() {
   return (
     <>
       <div className='miscPage'>
-        <h1>Version 001</h1>
+        <h1>Version 00.01.01</h1>
         <h2>Other useful text</h2>
       </div>
     </>
